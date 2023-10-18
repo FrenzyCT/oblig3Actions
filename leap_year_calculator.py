5d135e42cea2059464e80904857ff6ecf5dfc803
FrenzyCT <111730227+FrenzyCT@users.noreply.github.com>
2023-10-19 00:32:53 +0200

    test

---
+++ leap_year_calculator.py
@@ -0,0 +1,18 @@
+
+def calculate_if_leap_year(leapyear):
+    if leapyear % 400 == 0:
+        return True
+    elif leapyear % 4 == 0:
+        if leapyear % 100 != 0:
+            return True
+        #return True
+    return False
+
+
+
+if __name__ == "__main__":
+    print("Type the year you want to check if it's a leap year: ")
+    year = int(input())
+
+    isLeapyear = calculate_if_leap_year(year)
+    print(f"Is {year} a leap year?: {isLeapyear}")
---
+++ test_leap_year.py
@@ -0,0 +1,26 @@
+import pytest
+from leap_year_calculator import calculate_if_leap_year
+
+class TestLeapYear():
+
+
+    def test_divide_by_4_is_leap_year(self):
+        year = 2000
+        assert(calculate_if_leap_year(year))
+
+
+    def test_divide_by_4_is_NOT_leap_year(self):
+        year1 = 2000
+        assert(calculate_if_leap_year(year1))
+
+    def test_divide_by_4_and_100_but_not_400_is_not_a_leap_year(self):
+        year = 1700
+        assert not calculate_if_leap_year(year)
+
+    def test_divided_by_400_is_leap_year(self):
+        year = 1200
+        assert(calculate_if_leap_year(year))
+
+    def test_divided_by_400_is_NOT_leap_year(self):
+        year = 1700
+        assert(calculate_if_leap_year(year))
