4ab59c2d84912951700196e90e4c30b331b3df70
FrenzyCT <111730227+FrenzyCT@users.noreply.github.com>
2023-10-19 10:19:03 +0200

    Ny opplastning test

--- test_leap_year.py
+++ test_leap_year.py
@@ -24,6 +24,3 @@ class TestLeapYear():
     def test_divided_by_400_is_NOT_leap_year(self):
         year = 1700
         assert(calculate_if_leap_year(year))
-    def test_divided_by_400_is_NOT_leap_year(self):
-        year = 1700
-        assert(calculate_if_leap_year(year))
